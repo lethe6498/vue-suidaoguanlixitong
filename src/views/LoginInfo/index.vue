af5857758e8ff6a1cd4a90c3028bb5e6fec4967f
Chao <chao@ChaodeMacBook-Pro.local>
2025-10-22 08:43:20 +0800

    login页面

--- src/views/LoginInfo/index.vue
+++ src/views/LoginInfo/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
-    <h3>登陆页面</h3>
+  <div class="login-container">
+    <el-form class="user" size="large" :model="user">
+      <div class="title-container">
+        <h3 class="title">隧道后台管理系统登陆</h3>
+      </div>
+      <el-form-item prop="username">
+        <el-input
+          v-model="user.username"
+          type="text"
+          placeholder="请输入用户名"
+          name="username"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="user.password"
+          type="text"
+          placeholder="请输入密码"
+          name="password"
+        ></el-input>
+      </el-form-item>
+      <!--      .native.prevent阻止表单事件 -->
+      <el-button
+        style="width: 100%; margin-bottom: 30px"
+        type="primary"
+        @click.native.prevent="handleLogin"
+        >登陆</el-button
+      >
+    </el-form>
   </div>
 </template>
+<script setup>
+import { reactive } from 'vue'
+const user = reactive({
+  username: '',
+  password: '',
+})
+</script>
