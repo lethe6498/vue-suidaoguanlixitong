42c69c4e937766c79731a84889912d6ed90b97f6
chaozai <wangzaichao@outlook.com>
2025-10-27 13:40:23 +0800

    get服务器增加

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const router = require('./router')
+app.use('/api', router)
 //监听一个叫3000的端口
 app.listen(3000, () => {
   console.log('服务器运行在3000端口上')
--- server/router.js
+++ server/router.js
@@ -1,1 +1,10 @@
-
+const express = require('express')
+const router = express.Router()
+//添加接口
+router.get('/list', (req, res) => {
+  res.send({
+    status: 200,
+    messages: '测试服务器',
+  })
+})
+module.exports = router
