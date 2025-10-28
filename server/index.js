d717c972ed0fe81aa257e331143f475f61f2914c
chao <chao@chaodeMacBook-Pro.local>
2025-10-28 09:06:43 +0800

    feat: 更新服务器配置和依赖，优化隧道管理系统后端

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const router = require('./router')
+//解决跨域
+const cors = require('cors')
+app.use(cors())
+//接受post参数
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
 app.use('/api', router)
 //监听一个叫3000的端口
 app.listen(3000, () => {
--- server/router.js
+++ server/router.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const url = require('url')
+
 //添加接口
 router.get('/list', (req, res) => {
   res.send({
