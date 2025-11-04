7975621b83519511af6254e0baf2967d9455ce7d
lethe6498 <wangzaichao98@gmail.com>
2025-11-04 22:57:10 +0800

    路由权限

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../views/Homeview/index.vue'
 // 导入布局组件 - 作为主要的页面容器
 import Layout from '../views/Layout.vue'
 import LoginInfo from '../views/LoginInfo/index.vue'
+import { useLoginStore } from '../stores/loginStore.js'
 
 /**
  * 创建路由器实例
@@ -32,6 +33,11 @@ const router = createRouter({
           path: '/',
           name: 'Home',
           component: Home, // 直接导入，提高首屏加载速度
+          /*如果用户未登录，就跳转到登录页；
+如果已登录，才允许访问这个页面  */
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 建设管理页面
@@ -39,40 +45,76 @@ const router = createRouter({
           name: 'buildManage',
           // 使用动态导入实现路由级代码分割，提高应用性能
           component: () => import('../views/BuildManage/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 地质信息页面
           path: '/geological-info',
           name: 'geologicalInfo',
           component: () => import('../views/GeologicalInfo/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 项目信息页面
           path: '/project-info',
           name: 'projectInfo',
           component: () => import('../views/ProjectInfo/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 系统管理页面
           path: '/system-manage',
           name: 'systemManage',
           component: () => import('../views/SystemManage/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 隧道信息页面
           path: '/tunnel-info',
           name: 'tunnelInfo',
           component: () => import('../views/TunnelInfo/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
         {
           // 工作管理页面
           path: '/work-manage',
           name: 'workManage',
           component: () => import('../views/WorkManage/index.vue'),
+          meta: {
+            requiresAuth: true,
+          },
         },
       ],
     },
   ],
 })
-
+/* 路由权限 */
+//前置钩子函数
+useLoginStore
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    //验证是否登录
+    const loginStore = useLoginStore()
+    if (!loginStore.token) {
+      //如果不存在用户信息则去到登录页面
+      next({
+        path: '/LoginInfo',
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
 export default router
